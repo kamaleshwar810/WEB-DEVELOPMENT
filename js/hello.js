200ac648ede72d2461d856118201ddf2f0a6efe4
kamaleshwar810 <68476776+kamaleshwar810@users.noreply.github.com>
2020-09-06 12:11:17 +0530

    Delete hello.js

--- js/hello.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-var hellotoj = {};
-var helloword = "Hello";
-hellotoj.func = function (name) {
-  console.log(helloword + " " + name);
-}
-window.hellotoj = hellotoj;	
-
-})(window);
